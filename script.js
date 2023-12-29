1d08b70fe4efcf23214243911ad9a4936195bb3a
srijan <srijan.dhl21@gmail.com>
2023-12-29 22:50:54 +1100

    you are wel come my little bro (helpedwith my brother school project)

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const amount = document.getElementById('#cont');
-const percent = document.getElementById('#cont2');
-const btn = document.getElementById('#cont3');
-const total = document.getElementById('#cont4');
-
+const amount = document.getElementById('bill');
+const percent = document.getElementById('Tip');
+const btn = document.getElementById('buttn');
+const total = document.getElementById('total');
+// console.log(amount.value,percent.value);
 function calculate() {
 
     const amountvalue = amount.value;
     const percentvalue = percent.value;
-    const totalvalue = amountvalue * (1 + percentvalue / 100);
-    console.log(totalvalue);
+    const totalvalue = amountvalue * (percentvalue / 100);
+    total.innerText= 'Total:'+totalvalue
+
 }
 
 
